0935a14a49e309402b8fa6aef077e4439cfb26c1
cristiansavino <savinocristian89@gmail.com>
2021-02-12 00:54:00 +0000

    fixes

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,6 +10,7 @@
 <meta property="twitter:card" content="summary_large_image">
 <meta property="twitter:image:alt" content="Contador de palabras online">
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<link rel="shortcut icon" href="{{asset('favicon.ico')}}">
 
 <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
