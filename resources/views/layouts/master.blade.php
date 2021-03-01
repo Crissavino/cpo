6cd1700c9da95affab521e9a16cf415c86f56610
cristiansavino <savinocristian89@gmail.com>
2021-03-01 14:29:37 +0000

    change titles

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <title>@yield('title') - <b>Contador de Palabras Online</b></title>
+    <title>@yield('title') - Contador de Palabras Online</title>
     <meta name="description" content="@yield('description')">
     <meta property="og:description" content="@yield('description')">
     @include('partials.head')
