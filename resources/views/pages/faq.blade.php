7ac33595d85e2c86ee833c0bfec46757cc416475
cristiansavino <savinocristian89@gmail.com>
2021-03-09 11:41:50 +0000

    move text to faq v2

--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -20,85 +20,54 @@
         </a>
     </nav>
 
-    <h1 class="pb-5 titles text-center" style="color: white">FAQ</h1>
-
-    <section id="how-does-it-work" class="mb-5">
-        <h2 class="text-center titles">Como usar el contador de palabras online?</h2>
-        <div class="container" data-aos="zoom-out" data-aos-delay="150" data-aos-once="true">
-            <div class="row">
-                <div class="col-12 mt-4 text-center">
-                    <ul class="numbered-list mt-5">
-                        <li class="">
-                            <div>
-                                <p style="color: white; text-align: left">
-                                    El <strong>Contador de palabras online</strong> es una poderosa herramienta la cual te permite contar la cantidad de palabras, la cantidad de caracteres y letras, oraciones y párrafos que se encuentren en tu articulo, texto o ensayo. Solamente debes empezar a escribir en el cuadro de texto y nosotros nos encargaremos del resto. También es posible copiar y pegar tu texto y el sistema automáticamente mostrará el conteo de palabras y el conteo de caracteres como si lo hubieras escrito uno por uno.
-                                </p>
-                            </div>
-                        </li>
-                        <li class="mt-5">
-                            <div>
-                                <p style="color: white; text-align: left">
-                                    Saber cuantas palabras o caracteres posee tu documento puede ser de mucha utilidad. Suele suceder que a un autor se le pida un mínimo o un máximo de palabras para escribir un blog, una nota o artículo, y gracias el <strong>Contador de palabras online</strong> sabrás rápidamente si cumples con los requisitos.
-                                </p>
-                            </div>
-                        </li>
-                        <li class="mt-5">
-                            <div>
-                                <p style="color: white; text-align: left">
-                                    Además, te mostramos inmediatamente las diez palabras más utilizadas y la cantidad de veces que las repites en el ensayo que estes escribiendo. Esto te da la opción de saber cuales utilizas con más frecuencia y así podrás evitar utilizar en exceso ciertas palabras y te permitirá asegurarte que la distribución de las palabras claves coincide con lo que estás necesitando.
-                                </p>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section id="how-does-it-work" class="pb-5" data-aos="zoom-out" data-aos-delay="150" data-aos-once="true">
-        <h2 class="text-center titles">Como usar el simulador SERP online?</h2>
-        <div class="container">
-            <div class="row">
-                <div class="col-12 text-center">
-                    <ul class="numbered-list mt-5">
-                        <li class="">
-                            <div>
-                                <p style="color: white; text-align: left">
-                                    El <strong>Simulador SERP</strong> es una herramienta para que los dueños de sitios web puedan ver como lucirá su sitio en los resultados de búsqueda de Google. Esto te servirá para conocer si debes hacer algún ajuste, si el título se ve bien, si es muy larga la descripción, si la misma es relevante, entre otras tantas cosas.
-                                </p>
-                            </div>
-                        </li>
-                        <li class="mt-5">
-                            <div>
-                                <p style="color: white; text-align: left">
-                                    Solo toca el botón Empezar, escribe el título de tu web, la descripción de la página y la url del sitio. A su vez podrás usar nuestro contador de caracteres SEO que aparece a arriba a la derecha del titulo y la descripción para saber si lo que estas escribiendo no es demasiado largo para los resultados de las búsquedas de Google
-                                </p>
-                            </div>
-                        </li>
-                        <li class="mt-5">
-                            <div>
-                                <p style="color: white; text-align: left">
-                                    Cuando termines solo haz click en el botón Ver en google y nosotros nos encargaremos de mostrarte cómo se verá exactamente tu sitio en los resultados de búsqueda de Google cuando las personas te busquen.
-                                </p>
-                            </div>
-                        </li>
-                        <li class="mt-5">
-                            <div>
-                                <p style="color: white; text-align: left">
-                                </p>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </section>
-
     <section id="how-does-it-work" class="mb-5" style="min-height: 100vh">
+        <h1 class="pb-5 titles text-center" style="color: white">FAQ</h1>
         <div class="container-fluid" data-aos="zoom-out" data-aos-delay="150" data-aos-once="true">
             <div class="row">
                 <div class="col-12 text-center">
                     <div class="accordion numbered-list mt-5" id="accordionExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingSix">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="true" aria-controls="collapseSix">
+                                    Como usar el contador de palabras online?
+                                </button>
+                            </h2>
+                            <div id="collapseSix" class="accordion-collapse collapse show" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    <div class="row">
+                                        <p style="color: white; text-align: left">
+                                            El <strong>Contador de palabras online</strong> es una poderosa herramienta la cual te permite contar la cantidad de palabras, la cantidad de caracteres y letras, oraciones y párrafos que se encuentren en tu articulo, texto o ensayo. Solamente debes empezar a escribir en el cuadro de texto y nosotros nos encargaremos del resto. También es posible copiar y pegar tu texto y el sistema automáticamente mostrará el conteo de palabras y el conteo de caracteres como si lo hubieras escrito uno por uno.
+                                        </p>
+                                        <p style="color: white; text-align: left">
+                                            Saber cuantas palabras o caracteres posee tu documento puede ser de mucha utilidad. Suele suceder que a un autor se le pida un mínimo o un máximo de palabras para escribir un blog, una nota o artículo, y gracias el <strong>Contador de palabras online</strong> sabrás rápidamente si cumples con los requisitos.
+                                        </p>
+                                        <p style="color: white; text-align: left">
+                                            Además, te mostramos inmediatamente las diez palabras más utilizadas y la cantidad de veces que las repites en el ensayo que estes escribiendo. Esto te da la opción de saber cuales utilizas con más frecuencia y así podrás evitar utilizar en exceso ciertas palabras y te permitirá asegurarte que la distribución de las palabras claves coincide con lo que estás necesitando.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingSeven">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="true" aria-controls="collapseSeven">
+                                    Como usar el simulador SERP online?
+                                </button>
+                            </h2>
+                            <div id="collapseSeven" class="accordion-collapse collapse show" aria-labelledby="headingSeven" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    <p style="color: white; text-align: left">
+                                        El <strong>Simulador SERP</strong> es una herramienta para que los dueños de sitios web puedan ver como lucirá su sitio en los resultados de búsqueda de Google. Esto te servirá para conocer si debes hacer algún ajuste, si el título se ve bien, si es muy larga la descripción, si la misma es relevante, entre otras tantas cosas.
+                                    </p>
+                                    <p style="color: white; text-align: left">
+                                        Solo toca el botón Empezar, escribe el título de tu web, la descripción de la página y la url del sitio. A su vez podrás usar nuestro contador de caracteres SEO que aparece a arriba a la derecha del titulo y la descripción para saber si lo que estas escribiendo no es demasiado largo para los resultados de las búsquedas de Google
+                                    </p>
+                                    <p style="color: white; text-align: left">
+                                        Cuando termines solo haz click en el botón Ver en google y nosotros nos encargaremos de mostrarte cómo se verá exactamente tu sitio en los resultados de búsqueda de Google cuando las personas te busquen.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="headingOne">
                                 <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
