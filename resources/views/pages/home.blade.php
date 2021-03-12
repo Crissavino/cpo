41fa35005313cd540c26b0b0930f720bafd20537
cristiansavino <savinocristian89@gmail.com>
2021-03-12 08:52:46 +0000

    add text in home and serp again

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,7 +10,8 @@
         <div class="wrapper">
             <div class="m-5 text-center">
                 <h1 class="titles mb-5">CONTADOR DE PALABRAS ONLINE</h1>
-                <a onclick="goToCountWords()" id="startButton" class="btn btn-outline-light btn-no-fill btn-rounded mt-5 animate__animated custom-font">Empezar</a>
+                <a onclick="goToCountWords()" id="startButton"
+                   class="btn btn-outline-light btn-no-fill btn-rounded mt-5 animate__animated custom-font">Empezar</a>
             </div>
         </div>
     </header>
@@ -40,7 +41,8 @@
         <div class="animate timer-count text-center d-flex flex-column align-items-center">
             <span class="p-3" id="chronotime">0:00:00:00</span>
             <form name="chronoForm">
-                <button type="button" name="startstop" class="btn btn-primary mb-2" onClick="chronoStart()">Iniciar</button>
+                <button type="button" name="startstop" class="btn btn-primary mb-2" onClick="chronoStart()">Iniciar
+                </button>
                 <button type="button" name="reset" class="btn btn-danger" onClick="chronoReset()">Reiniciar</button>
             </form>
         </div>
@@ -53,34 +55,24 @@
 
             <section class="stats pb-5" data-aos="zoom-out" data-aos-delay="150" data-aos-once="true">
                 <article class="d-flex align-items-center">
-                    <div>
-                        <span class="num" id="numWords">0</span>
-                        <span class="label" style="width: 80px">Palabras</span>
-                    </div>
+                    <span class="num" id="numWords">0</span>
+                    <span class="label" style="width: 80px">Palabras</span>
                 </article>
                 <article class="d-flex align-items-center">
-                    <div>
-                        <span class="num" id="numCharsWithSpaces">0</span>
-                        <span class="label" style="width: 140px">Caracteres (con espacios)</span>
-                    </div>
+                    <span class="num" id="numCharsWithSpaces">0</span>
+                    <span class="label" style="width: 140px">Caracteres (con espacios)</span>
                 </article>
                 <article class="d-flex align-items-center">
-                    <div>
-                        <span class="num" id="numCharsWithoutSpaces">0</span>
-                        <span class="label" style="width: 130px">Caracteres (sin espacios)</span>
-                    </div>
+                    <span class="num" id="numCharsWithoutSpaces">0</span>
+                    <span class="label" style="width: 130px">Caracteres (sin espacios)</span>
                 </article>
                 <article class="d-flex align-items-center">
-                    <div>
-                        <span class="num" id="numParagraph">0</span>
-                        <span class="label" style="width: 80px">Párrafos</span>
-                    </div>
+                    <span class="num" id="numParagraph">0</span>
+                    <span class="label" style="width: 80px">Párrafos</span>
                 </article>
                 <article class="d-flex align-items-center">
-                    <div>
-                        <span class="num" id="numSentences">0</span>
-                        <span class="label" style="width: 80px">Oraciones</span>
-                    </div>
+                    <span class="num" id="numSentences">0</span>
+                    <span class="label" style="width: 80px">Oraciones</span>
                 </article>
             </section>
 
@@ -135,6 +127,52 @@
         </div>
 
     </div>
+
+    <section id="how-does-it-work" class="mb-5">
+        <h2 class="text-center titles">Como usar el contador de palabras online?</h2>
+        <div class="container" data-aos="zoom-out" data-aos-delay="150" data-aos-once="true">
+            <div class="row">
+                <div class="col-12 mt-4 text-center">
+                    <ul class="numbered-list mt-5">
+                        <li class="">
+                            <div>
+                                <p style="color: white; text-align: left">
+                                    El <strong>Contador de palabras online</strong> es una poderosa herramienta la cual
+                                    te permite contar la cantidad de palabras, la cantidad de caracteres y letras,
+                                    oraciones y párrafos que se encuentren en tu articulo, texto o ensayo. Solamente
+                                    debes empezar a escribir en el cuadro de texto y nosotros nos encargaremos del
+                                    resto. También es posible copiar y pegar tu texto y el sistema automáticamente
+                                    mostrará el conteo de palabras y el conteo de caracteres como si lo hubieras escrito
+                                    uno por uno.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="mt-5">
+                            <div>
+                                <p style="color: white; text-align: left">
+                                    Saber cuantas palabras o caracteres posee tu documento puede ser de mucha utilidad.
+                                    Suele suceder que a un autor se le pida un mínimo o un máximo de palabras para
+                                    escribir un blog, una nota o artículo, y gracias el <strong>Contador de palabras
+                                        online</strong> sabrás rápidamente si cumples con los requisitos.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="mt-5">
+                            <div>
+                                <p style="color: white; text-align: left">
+                                    Además, te mostramos inmediatamente las diez palabras más utilizadas y la cantidad
+                                    de veces que las repites en el ensayo que estes escribiendo. Esto te da la opción de
+                                    saber cuales utilizas con más frecuencia y así podrás evitar utilizar en exceso
+                                    ciertas palabras y te permitirá asegurarte que la distribución de las palabras
+                                    claves coincide con lo que estás necesitando.
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
 
 @endsection
 @section('javascript')
