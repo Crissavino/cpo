6732eac8877bfc9358f9becbd2e0b3c8acf82aba
cristiansavino <savinocristian89@gmail.com>
2021-03-17 13:04:44 +0000

    add articles WIP

---
+++ resources/views/partials/blog.blade.php
@@ -0,0 +1,80 @@
+<section id="blog" class="mb-5">
+    <h2 class="text-center titles mb-5">Lectura rápida</h2>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="card mb-3 col-10">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img
+                            class="ms-4 mt-4"
+                            src="{{asset('img/article-1.png')}}"
+                            alt="imagen donde aparece un cartel que dice bidrios, lo escribimos mal pero los colocamos bien"
+                            style="border-radius: .25rem"
+                        >
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Cual es la importancia de tener una buena ortografía?</h5>
+                            <p class="card-text">
+                                Es cierto que hoy en dia casi todo nuestro trabajo lo realizamos tecleando a toda velocidad en nustros dispositivos para así poder responder cada vez mas rapido los mensajes o correos que recibimos.
+                            </p>
+                            <p class="card-text">
+                                Y esto, claramente, no hace mas que incrementar nuestra posibilidad de cometer errores o que el autocorrector, bendito autocorrector, coloque cualquier palabra como seguro le ha pasado a la mayoria.
+                            </p>
+                            <p class="card-text">
+                                Una encuesta del sitio de citas <a href="https://match.com/">Match.com</a> a 5.500 solteros estadounidenses reveló que el 39% eran juzgados por su manejo de la grámatica
+                            </p>
+                            <p class="card-text">
+                                Esto se valoraba mas que el estilo de vestir, la sonrisa o el estado de sus dientes.
+                            </p>
+                            <p>
+                                Tal como adaptamos nuestra escritura si somos entrevistados para un trabajo, si estamos escribiendo un artículo para una clase o si simplementa estamos charlando con amigos, de la misma manera debemos ajustarnos en el mundo digital.
+                            </p>
+                            <p class="card-text"><small class="text-muted">Artículo completo <a href="https://www.bbc.com/mundo/vert-cap-40959475" target="_blank">aquí</a></small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-3 col-10">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="..." alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">
+                                This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                                This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                                This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                            </p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-3 col-10">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="..." alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">
+                                This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                                This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                                This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                            </p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
