fe69f114aa3cd0d58e44c4e033a20d4e5d8787ba
cristiansavino <savinocristian89@gmail.com>
2021-03-21 10:09:12 +0000

    change sitemap

--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 
-@section('title', 'Preguntas Frcuentes')
+@section('title', 'Preguntas Frecuentes')
 @section('description', 'Aqui encontraras todas las preguntas y respuestas que habitualmente suelen tener nuestros usuarios')
 @section('canonicalUrl', 'https://www.contadordepalabrasonline.com/faq')
 
